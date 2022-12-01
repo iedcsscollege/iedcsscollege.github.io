edb2031cd83b408f38a2c1efc30fe067d4ae54c7
m3tasploit <abdulmuhaiminmunu@gmail.com>
2022-12-01 16:34:22 +0530

    add gh-pages config

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -3,9 +3,9 @@ import type { GatsbyConfig } from "gatsby";
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `iedcsscollege.github.io`,
-    siteUrl: `iedcsscollege.github.io/webdev`,
+    siteUrl: `https://www.iedcsscollege.github.io/webdev`,
   },
-  pathPrefix:"/webdev",
+  pathPrefix: "/webdev",
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
