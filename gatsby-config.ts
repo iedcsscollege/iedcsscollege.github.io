645ca08051e56408fa7318919323f6b252c00747
m3tasploit <abdulmuhaiminmunu@gmail.com>
2022-12-19 21:36:18 +0530

    pathPrefix updated

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -5,7 +5,7 @@ const config: GatsbyConfig = {
     title: `iedcsscollege.github.io`,
     siteUrl: `https://iedcsscollege.github.io`,
   },
-  pathPrefix: "/webdev",
+  pathPrefix: "/",
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
